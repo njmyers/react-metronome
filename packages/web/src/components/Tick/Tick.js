c3331f2fa966d943e901b1f08d0ebe940e355420
njmyers <17010801+njmyers@users.noreply.github.com>
2018-09-24 10:46:42 -0400

    added web audio api and precise audio event scheduling

--- packages/web/src/components/Tick/Tick.js
+++ packages/web/src/components/Tick/Tick.js
@@ -1,37 +1,145 @@
 // @flow
 import * as React from 'react';
 import tick from '@metronome/assets/click.mp3';
-import withBeats from '@metronome/components/src/Controls/with-beats';
+import withClock from '@metronome/components/src/Controls/with-clock';
 
 type Props = {
+  ms: number,
+  counter: number,
   running: boolean,
   beat: number,
   beats: number,
 };
 
-class Tick extends React.Component {
-  tick = new Audio(tick);
-  tock = new Audio(tick);
+type State = {
+  AudioContext: null | AudioContext,
+  tick: null | ArrayBuffer,
+  tock: null | ArrayBuffer,
+  loaded: boolean,
+  scheduled: boolean,
+};
 
-  play = () => {
-    // reset state of all audio object before playing
-    if (this.props.beats % this.props.beat === 0) {
-      this.tick.pause();
-      this.tick.currentTime = 0;
-      this.tick.play();
+function webAudioTouchUnlock(context) {
+  return new Promise(function(resolve, reject) {
+    if (context.state === 'suspended' && 'ontouchstart' in window) {
+      var unlock = function() {
+        context.resume().then(
+          function() {
+            document.body.removeEventListener('touchstart', unlock);
+            document.body.removeEventListener('touchend', unlock);
+
+            resolve(true);
+          },
+          function(reason) {
+            reject(reason);
+          }
+        );
+      };
+
+      document.body.addEventListener('touchstart', unlock, false);
+      document.body.addEventListener('touchend', unlock, false);
     } else {
-      this.tock.pause();
-      this.tock.currentTime = 0;
-      this.tock.play();
+      resolve(false);
+    }
+  });
+}
+
+class Tick extends React.Component<Props, State> {
+  state = {
+    AudioContext: null,
+    tick: null,
+    tock: null,
+    loaded: false,
+    scheduled: false,
+  };
+
+  getLookahead = () => (this.props.ms - this.props.counter) / 1000;
+
+  play = (buffer: AudioBuffer) => {
+    if (this.state.AudioContext) {
+      const currentTime = this.state.AudioContext.currentTime;
+      console.log(currentTime, currentTime + this.getLookahead());
+      const source = this.state.AudioContext.createBufferSource();
+
+      source.buffer = buffer;
+      source.connect(this.state.AudioContext.destination);
+      source.start(currentTime + this.getLookahead());
+
+      this.setState({
+        scheduled: true,
+      });
     }
   };
 
-  componentDidUpdate(prevProps: Props) {
-    if (this.props.beats !== prevProps.beats && this.props.beats !== 0) {
-      if (process.env.NODE_ENV !== 'production') {
-        console.log('tick');
+  createAudioContext = () => {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    this.setState({
+      AudioContext: new AudioContext(),
+    });
+  };
+
+  loadAudioAsync = (url: string, name: string) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = () => {
+      if (this.state.AudioContext) {
+        this.state.AudioContext.decodeAudioData(request.response, (buffer) => {
+          this.setState({
+            [name]: buffer,
+            loaded: true,
+          });
+        });
       }
-      this.play();
+    };
+
+    request.onerror = (error) => {
+      console.log(error);
+    };
+
+    request.send();
+  };
+
+  shouldSchedule = (lookAhead = 50) => {
+    return (
+      this.props.ms - this.props.counter < lookAhead && !this.state.scheduled
+    );
+  };
+
+  log = (string: string) => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(string);
+    }
+  };
+
+  componentDidMount() {
+    this.createAudioContext();
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.shouldSchedule()) {
+      if (this.props.beat % this.props.beats !== 0) {
+        this.log('tick');
+        this.play(this.state.tick);
+      } else {
+        this.log('tock');
+        this.play(this.state.tock);
+      }
+    }
+
+    // when beat hits remove scheduled flag
+    if (prevProps.beats !== this.props.beats) {
+      this.setState({
+        scheduled: false,
+      });
+    }
+
+    if (this.state.AudioContext && !prevState.AudioContext) {
+      webAudioTouchUnlock(this.state.AudioContext);
+      this.loadAudioAsync(tick, 'tick');
+      this.loadAudioAsync(tick, 'tock');
     }
   }
 
@@ -40,4 +148,4 @@ class Tick extends React.Component {
   }
 }
 
-export default withBeats(Tick);
+export default withClock(Tick);
--- packages/web/src/pages/WebApp.js
+++ packages/web/src/pages/WebApp.js
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import Metronome from '../components/Metronome';
-import AppLink from '../components/AppLink';
+// import AppLink from '../components/AppLink';
 import 'normalize.css';
 
-const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
-
-const WebApp = () => (!iOS ? <Metronome /> : <AppLink />);
-
-export default WebApp;
+export default () => <Metronome />;
